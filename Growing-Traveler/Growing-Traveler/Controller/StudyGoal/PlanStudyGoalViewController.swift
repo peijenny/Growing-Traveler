83e9aa51dbedd75e8e6c2f8c5b5492f1848942fa
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-09 15:59:38 +0800

    Add select calendar date behavior

--- Growing-Traveler/Growing-Traveler/Controller/StudyGoal/PlanStudyGoalViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/StudyGoal/PlanStudyGoalViewController.swift
@@ -7,6 +7,24 @@
 
 import UIKit
 
+enum SelectDateType {
+    
+    case startDate
+    
+    case endDate
+    
+    var title: String {
+        
+        switch self {
+            
+        case .startDate: return "start"
+            
+        case .endDate: return "end"
+            
+        }
+    }
+}
+
 class PlanStudyGoalViewController: UIViewController {
 
     @IBOutlet weak var planStudyGoalTableView: UITableView! {
@@ -20,6 +38,14 @@ class PlanStudyGoalViewController: UIViewController {
         }
 
     }
+    
+    let selectCalenderViewController = SelectCalendarViewController()
+    
+    var formatter = DateFormatter()
+    
+    var selectDate = Date()
+    
+    var selectType = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -85,19 +111,76 @@ extension PlanStudyGoalViewController: UITableViewDelegate, UITableViewDataSourc
             for: .touchUpInside
         )
         
+        formatter.dateFormat = "yyyy.MM.dd"
+        
+        if selectType == SelectDateType.startDate.title {
+            
+            headerView.startDateLabel.text = formatter.string(from: selectDate)
+            
+        } else if selectType == SelectDateType.endDate.title {
+            
+            headerView.endDateLabel.text = formatter.string(from: selectDate)
+            
+        }
+        
         return headerView
 
     }
     
     @objc func selectStartDateButton(sender: UIButton) {
         
-        // 跳出選擇開始日期的 Calender
+        selectCalenderViewController.startDate = Date()
+        
+        selectCalenderViewController.getSelectDate = { [weak self] date in
+            
+            self?.selectDate = date
+            
+            self?.selectType = SelectDateType.startDate.title
+            
+            self?.planStudyGoalTableView.reloadData()
+            
+        }
+        
+        setSelectCalenderViewController()
         
     }
     
     @objc func selectEndDateButton(sender: UIButton) {
         
-        // 跳出選擇結束日期的 Calender
+        selectCalenderViewController.startDate = selectDate
+        
+        selectCalenderViewController.getSelectDate = { [weak self] date in
+            
+            self?.selectDate = date
+            
+            self?.selectType = SelectDateType.endDate.title
+            
+            self?.planStudyGoalTableView.reloadData()
+            
+        }
+        
+        setSelectCalenderViewController()
+        
+    }
+    
+    func setSelectCalenderViewController() {
+        
+        selectCalenderViewController.view.backgroundColor = UIColor.systemGray5
+        
+        view.addSubview(selectCalenderViewController.view)
+        
+        selectCalenderViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            selectCalenderViewController.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            selectCalenderViewController.view.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            selectCalenderViewController.view.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+            selectCalenderViewController.view.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4)
+        ])
+
+        self.addChild(selectCalenderViewController)
+        
+        selectCalenderViewController.calendar.reloadData()
         
     }
     
---
+++ Growing-Traveler/Growing-Traveler/Controller/StudyGoal/SelectCalendarViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SelectCalenderViewController.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/9.
+//
+
+import UIKit
+import FSCalendar
+
+class SelectCalendarViewController: UIViewController {
+    
+    var calendar = FSCalendar()
+    
+    var getSelectDate: ((_ date: Date) -> Void)?
+    
+    var startDate = Date()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setCalenderFrame()
+        
+        view.backgroundColor = UIColor.white
+        
+        // 左右滑動
+        calendar.scrollDirection = .horizontal
+        
+        // 修改 weekendColor
+        calendar.appearance.titleWeekendColor = UIColor.lightGray
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        calendar.delegate = self
+        
+        calendar.dataSource = self
+    }
+
+    func setCalenderFrame() {
+        
+        view.addSubview(calendar)
+        
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            calendar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            calendar.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            calendar.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9),
+            calendar.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9)
+        ])
+        
+    }
+
+}
+
+
+extension SelectCalendarViewController:  FSCalendarDelegate, FSCalendarDataSource {
+    
+    // MARK: - Calendar DataSource
+    
+    func minimumDate(for calendar: FSCalendar) -> Date {
+        
+        return startDate
+        
+    }
+    
+    // MARK: - Calendar Delegate
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
+        getSelectDate?(date)
+        
+        self.view.removeFromSuperview()
+        
+    }
+    
+}
