f924dbc935233590c5f8582edf8ea15c6f613349
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-09 13:28:51 +0800

    Create PlanStudyGoalHeaderView UI and PlanStudyGoalVC register

---
+++ Growing-Traveler/Growing-Traveler/View/StudyGoal/PlanStudyGoalHeaderView.swift
@@ -0,0 +1,35 @@
+//
+//  HeaderView.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/9.
+//
+
+import UIKit
+
+class PlanStudyGoalHeaderView: UITableViewHeaderFooterView {
+  
+
+    @IBOutlet weak var studyGoalTitleTextField: UITextField!
+    
+    @IBOutlet weak var startDateLabel: UILabel!
+    
+    @IBOutlet weak var startDateCalenderButton: UIButton!
+    
+    @IBOutlet weak var endDateLabel: UILabel!
+    
+    @IBOutlet weak var endDateCalenderButton: UIButton!
+    
+    @IBOutlet weak var categoryTagLabel: UILabel!
+    
+    @IBOutlet weak var selectCategoryTagButton: UIButton!
+    
+    @IBOutlet weak var addStudyItemButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
