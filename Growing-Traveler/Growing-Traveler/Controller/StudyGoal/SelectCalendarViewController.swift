50018dee5c2a86d4267ccf4f5389f63c576d6197
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-10 16:57:03 +0800

    Modify SelectCalendarVC and moving between VC behavior

--- Growing-Traveler/Growing-Traveler/Controller/StudyGoal/SelectCalendarViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/StudyGoal/SelectCalendarViewController.swift
@@ -14,7 +14,17 @@ class SelectCalendarViewController: UIViewController {
     
     var getSelectDate: ((_ date: Date) -> Void)?
     
-    var startDate = Date()
+    var startDate: Date? {
+        
+        didSet {
+            
+            calendar.delegate = self
+            
+            calendar.dataSource = self
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,14 +40,6 @@ class SelectCalendarViewController: UIViewController {
         calendar.appearance.titleWeekendColor = UIColor.lightGray
 
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        calendar.delegate = self
-        
-        calendar.dataSource = self
-    }
 
     func setCalenderFrame() {
         
@@ -48,8 +50,8 @@ class SelectCalendarViewController: UIViewController {
         NSLayoutConstraint.activate([
             calendar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             calendar.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            calendar.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9),
-            calendar.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9)
+            calendar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            calendar.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9)
         ])
         
     }
@@ -62,7 +64,7 @@ extension SelectCalendarViewController: FSCalendarDelegate, FSCalendarDataSource
     
     func minimumDate(for calendar: FSCalendar) -> Date {
         
-        return startDate
+        return startDate ?? Date()
         
     }
     
@@ -72,7 +74,7 @@ extension SelectCalendarViewController: FSCalendarDelegate, FSCalendarDataSource
         
         getSelectDate?(date)
         
-        self.view.removeFromSuperview()
+        dismiss(animated: true, completion: .none)
         
     }
     
