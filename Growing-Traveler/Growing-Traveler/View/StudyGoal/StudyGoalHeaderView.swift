da23f9c2c556b696097a7e2313e2419745ff2184
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-11 16:00:23 +0800

    Add IBOutlet in class

--- Growing-Traveler/Growing-Traveler/View/StudyGoal/StudyGoalHeaderView.swift
+++ Growing-Traveler/Growing-Traveler/View/StudyGoal/StudyGoalHeaderView.swift
@@ -9,12 +9,8 @@ import UIKit
 
 class StudyGoalHeaderView: UITableViewHeaderFooterView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var studyGoalTitleLabel: UILabel!
+    
+    @IBOutlet weak var endDateLabel: UILabel!
+    
 }
--- Growing-Traveler/Growing-Traveler/View/StudyGoal/StudyGoalTableViewCell.swift
+++ Growing-Traveler/Growing-Traveler/View/StudyGoal/StudyGoalTableViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class StudyGoalTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var checkButton: UIButton!
+    
+    @IBOutlet weak var studyItemLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
