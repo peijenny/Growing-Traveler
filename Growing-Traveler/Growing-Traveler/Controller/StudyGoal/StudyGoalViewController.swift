a8b2bc57da2cc1e38e63beab32b026b0b9ee04e0
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-11 15:39:05 +0800

    Register tableViewCell in studyGoalTableView

--- Growing-Traveler/Growing-Traveler/Controller/StudyGoal/StudyGoalViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/StudyGoal/StudyGoalViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class StudyGoalViewController: UIViewController {
     
+    @IBOutlet weak var studyGoalTableView: UITableView! {
+        
+        didSet {
+            
+            studyGoalTableView.delegate = self
+            
+            studyGoalTableView.dataSource = self
+            
+        }
+        
+    }
+    
     @IBOutlet weak var addGoalButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        studyGoalTableView.register(
+            UINib(nibName: String(describing: StudyGoalHeaderView.self), bundle: nil),
+            forHeaderFooterViewReuseIdentifier: String(describing: StudyGoalHeaderView.self)
+        )
+        
+        studyGoalTableView.register(
+            UINib(nibName: String(describing: StudyGoalFooterView.self), bundle: nil),
+            forHeaderFooterViewReuseIdentifier: String(describing: StudyGoalFooterView.self)
+        )
+        
+        studyGoalTableView.register(
+            UINib(nibName: String(describing: StudyGoalTableViewCell.self), bundle: nil),
+            forCellReuseIdentifier: String(describing: StudyGoalTableViewCell.self)
+        )
 
     }
     
@@ -38,6 +65,56 @@ class StudyGoalViewController: UIViewController {
         
         navigationController?.pushViewController(viewController, animated: true)
         
+    }
+    
+}
+
+extension StudyGoalViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 2
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: StudyGoalTableViewCell.self),
+            for: indexPath
+        )
+
+        guard let cell = cell as? StudyGoalTableViewCell else { return cell }
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: String(describing: StudyGoalHeaderView.self))
+
+        guard let headerView = headerView as? StudyGoalHeaderView else { return headerView }
+        
+        return headerView
+        
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        
+        let footerView = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: String(describing: StudyGoalFooterView.self))
+
+        guard let footerView = footerView as? StudyGoalFooterView else { return footerView }
+        
+        return footerView
     }
     
 }
