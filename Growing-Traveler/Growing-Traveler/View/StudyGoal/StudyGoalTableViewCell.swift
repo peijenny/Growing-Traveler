47ad170205f3e1e57a7d84680a189ba341ec5967
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-11 15:15:02 +0800

    Update podfile and addStudyTableViewCell

---
+++ Growing-Traveler/Growing-Traveler/View/StudyGoal/StudyGoalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  studyGoalTableViewCell.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/11.
+//
+
+import UIKit
+
+class StudyGoalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
