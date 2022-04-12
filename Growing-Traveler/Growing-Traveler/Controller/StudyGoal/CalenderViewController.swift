2b8f6eea0c50e51e90346b415f285b7db6f2c3f5
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-12 13:54:09 +0800

    Initiate CalendarVC

--- Growing-Traveler/Growing-Traveler/Controller/StudyGoal/CalenderViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/StudyGoal/CalenderViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
+import FSCalendar
 
 class CalenderViewController: UIViewController {
 
+    @IBOutlet weak var calendarView: FSCalendar! {
+        
+        didSet {
+            
+            calendarView.delegate = self
+            
+            calendarView.dataSource = self
+            
+        }
+        
+    }
+    
+    @IBOutlet weak var displayTableView: UITableView! {
+        
+        didSet {
+            
+            displayTableView.delegate = self
+            
+            displayTableView.dataSource = self
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension CalenderViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
+    // MARK: - Calendar DataSource
+    
+    func minimumDate(for calendar: FSCalendar) -> Date {
+        
+        return Date()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Calendar Delegate
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
+        // 顯示 Calender 的日期，顯示出當日需要學習的目標
+        
     }
-    */
+    
+}
 
+extension CalenderViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    
+    
 }
