776170db84157ad100745373dce6ea885a4973b5
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-13 13:14:03 +0800

    Create ForumArticle Object and ForumArticleManager class

---
+++ Growing-Traveler/Growing-Traveler/Model/Forum/ForumArticle.swift
@@ -0,0 +1,39 @@
+//
+//  ForumArticle.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/13.
+//
+
+import Foundation
+
+struct ForumArticle: Codable {
+    
+    var id: String
+    
+    var userID: String
+    
+    var createTime: TimeInterval
+    
+    var title: String
+    
+    var content: [ArticleContent]
+    
+}
+
+struct ArticleContent: Codable {
+    
+    var text: String?
+    
+    var image: String?
+}
+
+struct ArticleMessage: Codable {
+    
+    var userID: String
+    
+    var createTime: TimeInterval
+    
+    var message: [ArticleContent]
+    
+}
---
+++ Growing-Traveler/Growing-Traveler/Model/Forum/ForumArticleManager.swift
@@ -0,0 +1,16 @@
+//
+//  ForumArticleManager.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/13.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+class ForumArticleManager {
+    
+    let database = Firestore.firestore().collection("forum")
+    
+}
