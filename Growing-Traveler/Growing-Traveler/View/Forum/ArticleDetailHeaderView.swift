7c52857e3cb257eae0b63a0449c5fbfbf6ad4b90
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-15 19:36:56 +0800

    Create ArticleDetailHeaderView

---
+++ Growing-Traveler/Growing-Traveler/View/Forum/ArticleDetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleDetailHeaderView.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/15.
+//
+
+import UIKit
+
+class ArticleDetailHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
