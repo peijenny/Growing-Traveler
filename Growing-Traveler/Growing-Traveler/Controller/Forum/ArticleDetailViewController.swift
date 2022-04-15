ff46354fc3eb40880395f8fe32e1b37ad9d2b19b
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-15 19:18:45 +0800

    Add ArticleDetailVC and create VC in storyboard

---
+++ Growing-Traveler/Growing-Traveler/Controller/Forum/ArticleDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailViewController.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/15.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
