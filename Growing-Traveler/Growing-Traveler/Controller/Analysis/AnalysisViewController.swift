0a495b8cf9a62235e7e393dd4283bc3df4b4f648
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-20 15:49:37 +0800

    Test add BarChart in view

--- Growing-Traveler/Growing-Traveler/Controller/Analysis/AnalysisViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/Analysis/AnalysisViewController.swift
@@ -6,24 +6,55 @@
 //
 
 import UIKit
+import Charts
 
 class AnalysisViewController: UIViewController {
 
+    @IBOutlet weak var analysisBarChatView: BarChartView!
+    
+    // 測試顯示資料
+    var monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    
+    var temperatureArray: [Double] = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+    
+    var axisFormatDelgate: AxisValueFormatter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateChatsData()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateChatsData() {
+        
+        // 存放資料的陣列，型態為 BarChartDataEntry
+        var dataEntries: [BarChartDataEntry] = []
+        
+        // 使用迴圈將資料加入存放資料的陣列中
+        for index in 0..<monthArray.count {
+            
+            // 設定 x, y 座標要顯示的東西有哪些
+            let dataEntry = BarChartDataEntry(x: Double(index), y: temperatureArray[index])
+            
+            // 將資料加入到陣列中
+            dataEntries.append(dataEntry)
+            
+        }
+        
+        // 設定 BarChartDataSet 設定要顯示的資料是什麼，以及圖表下方的 Label
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Temperature per month")
+        
+        chartDataSet.colors = ChartColorTemplates.colorful()
+        
+        let chartData = BarChartData(dataSet: chartDataSet)
+        
+        analysisBarChatView.data = chartData
+        
+        analysisBarChatView.xAxis.valueFormatter = IndexAxisValueFormatter(values: monthArray)
+        
+        analysisBarChatView.xAxis.granularity = 1
+        
     }
-    */
 
 }
