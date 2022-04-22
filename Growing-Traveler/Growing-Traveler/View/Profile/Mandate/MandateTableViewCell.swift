526ed1bc940f0bc18effee67722ff555462acc7b
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-22 17:55:44 +0800

    Ignore file

---
+++ Growing-Traveler/Growing-Traveler/View/Profile/Mandate/MandateTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MandateTableViewCell.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/22.
+//
+
+import UIKit
+
+class MandateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
