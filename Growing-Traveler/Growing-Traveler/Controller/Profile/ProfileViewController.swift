1277f76dee6e61496fd98b5ff5d64afc15f2b529
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-22 16:20:26 +0800

    Create FeatureCollectionViewCell and register in ProfileVC and set DataSource

--- Growing-Traveler/Growing-Traveler/Controller/Profile/ProfileViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/Profile/ProfileViewController.swift
@@ -7,16 +7,129 @@
 
 import UIKit
 
+enum FeatureType {
+    
+    case mandate
+    
+    case rank
+    
+    case note
+    
+    case release
+    
+    case license
+    
+    var title: String {
+        
+        switch self {
+            
+        case .mandate: return "成長任務"
+            
+        case .rank: return "學習排行榜"
+            
+        case .note: return "學習筆記"
+            
+        case .release: return "發布紀錄"
+            
+        case .license: return "個人認證"
+            
+        }
+        
+    }
+    
+}
+
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var profileView: ProfileView!
     
     @IBOutlet weak var featureCollectionView: UICollectionView!
     
+    private var dataSource: UICollectionViewDiffableDataSource<Int, FeatureType>?
+    
+    let featureList: [FeatureType] = [.mandate, .rank, .note, .release, .license]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureDataSource()
+        
+    }
+    
+    func configureDataSource() {
+        
+        featureCollectionView.register(
+            UINib(nibName: String(describing: FeatureCollectionViewCell.self), bundle: nil),
+            forCellWithReuseIdentifier: String(describing: FeatureCollectionViewCell.self))
+        
+        dataSource = UICollectionViewDiffableDataSource.init(
+        collectionView: featureCollectionView, cellProvider: { collectionView, indexPath, item in
+            
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: String(describing: FeatureCollectionViewCell.self), for: indexPath)
+            
+            guard let cell = cell as? FeatureCollectionViewCell else { return cell }
+                
+            cell.featureItemLabel.text = self.featureList[indexPath.item].title
+            
+            return cell
+            
+        })
+        
+        featureCollectionView.dataSource = dataSource
+        
+        featureCollectionView.delegate = self
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, FeatureType>()
+        
+        snapshot.appendSections([0])
+        
+        snapshot.appendItems(featureList, toSection: 0)
+        
+        dataSource?.apply(snapshot, animatingDifferences: true)
+        
+        featureCollectionView.collectionViewLayout = generateLayout()
+        
+    }
+    
+    func generateLayout() -> UICollectionViewLayout {
+
+        let padding: Double = 10
+
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: padding, leading: padding, bottom: padding, trailing: padding)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
+        
+        return UICollectionViewCompositionalLayout(section: section)
+        
+//        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+//
+//        configuration.scrollDirection = .horizontal
+//
+//        return UICollectionViewCompositionalLayout(section: section, configuration: configuration)
+
     }
 
+}
+
+extension ProfileViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        print("TEST \(indexPath.item)")
+        
+    }
 }
---
+++ Growing-Traveler/Growing-Traveler/View/Profile/FeatureCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FeatureCollectionViewCell.swift
+//  Growing-Traveler
+//
+//  Created by Jenny Hung on 2022/4/22.
+//
+
+import UIKit
+
+class FeatureCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var featureItemLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
