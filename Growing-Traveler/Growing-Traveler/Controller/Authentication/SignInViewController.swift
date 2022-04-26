711206162c6c7be5894a27e15073a3f9a3f88ef5
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-26 19:03:29 +0800

    Show tableViewCell in SignVC and modify UI

--- Growing-Traveler/Growing-Traveler/Controller/Authentication/SignInViewController.swift
+++ Growing-Traveler/Growing-Traveler/Controller/Authentication/SignInViewController.swift
@@ -9,19 +9,78 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
-    @IBOutlet weak var signTableView: UITableView!
-    
+    @IBOutlet weak var signTableView: UITableView! {
+        
+        didSet {
+            
+            signTableView.delegate = self
+            
+            signTableView.dataSource = self
+            
+        }
+        
+    }
+   
     var signType = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        signTableView.register(
+            UINib(nibName: String(describing: SignInTableViewCell.self), bundle: nil),
+            forCellReuseIdentifier: String(describing: SignInTableViewCell.self)
+        )
+        
+        signTableView.register(
+            UINib(nibName: String(describing: SignUpTableViewCell.self), bundle: nil),
+            forCellReuseIdentifier: String(describing: SignUpTableViewCell.self)
+        )
+        
     }
 
     @IBAction func backAuthPage(_ sender: UIButton) {
         
         dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
+
+extension SignInViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if signType == SignType.signIn.title {
+            
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: SignInTableViewCell.self), for: indexPath)
+            
+            guard let cell = cell as? SignInTableViewCell else { return cell }
+            
+            return cell
+            
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: SignUpTableViewCell.self), for: indexPath)
+            
+            guard let cell = cell as? SignUpTableViewCell else { return cell }
+            
+            return cell
+            
+        }
         
     }
     
