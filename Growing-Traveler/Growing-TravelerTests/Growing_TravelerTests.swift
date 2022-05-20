ac526e7a931a115edc2853db453709c1b4e9e751
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-05-20 17:45:28 +0800

    Modify unit test

--- Growing-Traveler/Growing-TravelerTests/Growing_TravelerTests.swift
+++ Growing-Traveler/Growing-TravelerTests/Growing_TravelerTests.swift
@@ -17,6 +17,7 @@ class GrowingTravelerTests: XCTestCase {
         try super.setUpWithError()
         
         sut = HandleAnalysisManager()
+        
     }
 
     override func tearDownWithError() throws {
@@ -29,17 +30,16 @@ class GrowingTravelerTests: XCTestCase {
     
     func testTarget() {
         
-        let day = 24 * 60 * 60
+        // given
+        let yesterday = Date(timeIntervalSince1970: 1652976000)
         
-        let yesterday = Date()
-            .addingTimeInterval(-Double((day))).addingTimeInterval(Double(day / 3))
+        let sevenDaysArray = ["05.13", "05.14", "05.15", "05.16", "05.17", "05.18", "05.19"]
         
-        let sevenDaysArray = [
-            "05.13", "05.14", "05.15", "05.16", "05.17", "05.18", "05.19"]
-        
+        // when
         sut.handleSevenDays(yesterday: yesterday)
         
-        XCTAssertEqual(sut.sevenDaysArray, sevenDaysArray, "時間不是昨天")
+        // then
+        XCTAssertEqual(sut.sevenDaysArray, sevenDaysArray, "Time isn't yesterday.")
         
     }
     
@@ -47,30 +47,12 @@ class GrowingTravelerTests: XCTestCase {
         
         measure(metrics: [XCTClockMetric(), XCTCPUMetric(), XCTStorageMetric(), XCTMemoryMetric()]) {
             
-            let day = 24 * 60 * 60
-            
-            let yesterday = Date()
-                .addingTimeInterval(-Double((day))).addingTimeInterval(Double(day / 3))
+            let yesterday = Date(timeIntervalSince1970: 1652976000)
             
             sut.handleSevenDays(yesterday: yesterday)
             
         }
         
     }
-    
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//        // Any test you write for XCTest can be annotated as throws and async.
-//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
 
 }
